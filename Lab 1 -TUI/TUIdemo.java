143289a0d6e0af19b319985d5a8eaf6ec87132b2
Pilipenko Oleksandr <sanikpilipenko@gmail.com>
2026-05-27 17:47:47 +0300

    Update TUIdemo.java

--- Lab 1 -TUI/TUIdemo.java
+++ Lab 1 -TUI/TUIdemo.java
@@ -1,5 +1,6 @@
 package com.mybank.tui;
 
+import com.mybank.domain.*;
 import jexer.TAction;
 import jexer.TApplication;
 import jexer.TField;
@@ -8,16 +9,21 @@ import jexer.TWindow;
 import jexer.event.TMenuEvent;
 import jexer.menu.TMenu;
 
-/**
- *
- * @author Alexander 'Taurus' Babich
- */
 public class TUIdemo extends TApplication {
 
     private static final int ABOUT_APP = 2000;
     private static final int CUST_INFO = 2010;
 
     public static void main(String[] args) throws Exception {
+        
+        Bank bank = Bank.getBank();
+
+        bank.addCustomer("Іван", "Іванов");
+        bank.getCustomer(0).addAccount(new SavingsAccount(500.00, 0.03)); 
+
+        bank.addCustomer("Марія", "Петрова");
+        bank.getCustomer(1).addAccount(new CheckingAccount(2000.00, 100.00));
+
         TUIdemo tdemo = new TUIdemo();
         (new Thread(tdemo)).start();
     }
@@ -26,23 +32,18 @@ public class TUIdemo extends TApplication {
         super(BackendType.SWING);
 
         addToolMenu();
-        //custom 'File' menu
         TMenu fileMenu = addMenu("&File");
         fileMenu.addItem(CUST_INFO, "&Customer Info");
         fileMenu.addDefaultItem(TMenu.MID_SHELL);
         fileMenu.addSeparator();
         fileMenu.addDefaultItem(TMenu.MID_EXIT);
-        //end of 'File' menu  
 
         addWindowMenu();
 
-        //custom 'Help' menu
         TMenu helpMenu = addMenu("&Help");
         helpMenu.addItem(ABOUT_APP, "&About...");
-        //end of 'Help' menu 
 
         setFocusFollowsMouse(true);
-        //Customer window
         ShowCustomerDetails();
     }
 
@@ -65,16 +66,46 @@ public class TUIdemo extends TApplication {
 
         custWin.addLabel("Enter customer number: ", 2, 2);
         TField custNo = custWin.addField(24, 2, 3, false);
-        TText details = custWin.addText("Owner Name: \nAccount Type: \nAccount Balance: ", 2, 4, 38, 8);
+        TText details = custWin.addText("Owner Name: \nAccount Type: \nAccount Balance: ", 2, 4, 38, 4);
+        
         custWin.addButton("&Show", 28, 2, new TAction() {
             @Override
             public void DO() {
                 try {
                     int custNum = Integer.parseInt(custNo.getText());
-                    //details about customer with index==custNum
-                    details.setText("Owner Name: John Doe (id="+custNum+")\nAccount Type: 'Checking'\nAccount Balance: $200.00");
-                } catch (Exception e) {
+                    Bank bank = Bank.getBank();
+                    
+                    if (custNum >= 0 && custNum < bank.getNumberOfCustomers()) {
+                        Customer customer = bank.getCustomer(custNum);
+                        
+                        if (customer.getNumberOfAccounts() > 0) {
+                            Account account = customer.getAccount(0);
+
+                            String acctType = "Unknown";
+                            if (account instanceof SavingsAccount) {
+                                acctType = "Savings";
+                            } else if (account instanceof CheckingAccount) {
+                                acctType = "Checking";
+                            } else {
+                                acctType = "Account";
+                            }
+
+                            String info = String.format(
+                                "Owner Name: %s %s (id=%d)\nAccount Type: '%s'\nAccount Balance: $%.2f",
+                                customer.getFirstName(), customer.getLastName(), custNum, acctType, account.getBalance()
+                            );
+                            
+                            details.setText(info);
+                        } else {
+                            details.setText("Owner Name: " + customer.getFirstName() + " " + customer.getLastName() + "\nNo accounts found for this customer.");
+                        }
+                    } else {
+                        messageBox("Error", "Customer with ID " + custNum + " does not exist!").show();
+                    }
+                } catch (NumberFormatException e) {
                     messageBox("Error", "You must provide a valid customer number!").show();
+                } catch (Exception e) {
+                    messageBox("Error", "An unexpected error occurred: " + e.getMessage()).show();
                 }
             }
         });
